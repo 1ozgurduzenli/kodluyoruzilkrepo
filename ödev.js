cde97a02a5a9ab3304aca1565439895fc07d65df
1ozgurduzenli <106782532+1ozgurduzenli@users.noreply.github.com>
2022-06-05 18:39:08 +0300

    Add files via upload

---
+++ ödev.js
@@ -0,0 +1,10 @@
+const username = prompt('isim giriniz')
+if (username){
+console.log(`
+Merhaba, ${username}, hoş geldin!
+${Date.UTC8}tarihinde kodluyoruz Frontend Web Development 
+patikası'nın Javascript bölümü 1. ödevindesiniz.
+`)
+} else{
+    console.log('isim giriniz')
+}
